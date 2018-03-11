f9b1955eb7494a18ea221600c0e0042ebf0c58d6
Maciej Abramowski <m.abramowski@wp.pl>
2018-03-11 17:45:47 +0100

    database setup

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.static('./public'));
+app.use('/assets', express.static('./public'));
 
 toDoController(app);
 
--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -1,15 +1,39 @@
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//Connect to the database
+mongoose.connect('mongodb://test:test@ds111319.mlab.com:11319/todo');
+
+//Create a schema
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+  if (err) throw err;
+  console.log('item saved');
+});
+
+var Data = [{item: 'get milk'},{item:'walk dog'}, {item: 'do smth else'}];
+var urlencodedParser = bodyParser.urlencoded({extended:false});
+
 module.exports = function(app){
 
   app.get('/todo', function(req, res){
-
+    res.render('todo', {todos:Data});
   });
 
-  app.post('/todo', function(req, res){
-
+  app.post('/todo', urlencodedParser, function(req, res){
+    Data.push(req.body);
+    res.json(Data);
   });
 
-  app.delete('/todo', function(req, res){
-
+  app.delete('/todo/:item', function(req, res){
+     Data = Data.filter(function(todo){
+       return todo.item.replace(/ /g, '-') !== req.params.item;
+     });
+     res.json(Data);
   });
 
 };
